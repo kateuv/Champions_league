0363e7c56abc42afd78ac7c87eec6bb85a6606cd
kateuv <katie.uvanovic@outlook.com>
2023-03-03 20:36:43 +0000

    Added a query that checks which club has the most number of goals

--- stats.sql
+++ stats.sql
@@ -9,3 +9,14 @@ GROUP BY match_played
 ORDER BY assists DESC;
 
 
+SELECT player_name, t_won, tackles
+FROM defending
+WHERE position = "Midfielder" OR position = "Defender"
+GROUP BY tackles
+ORDER BY t_won DESC;
+
+/*Which club averaged the most goals*/
+SELECT club, SUM(goals) AS number_of_goals
+FROM goals
+GROUP BY club
+ORDER BY number_of_goals DESC;
